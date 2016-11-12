c0a18ac9e83f031cbd56a0de7221e289bb6ef3f1
TakahashiIkki <new_ittyandesu@outlook.jp>
2016-11-12 16:55:34 +0900

    add SlideMenuController

---
+++ Pods/Target Support Files/Pods-SampleSlideMenuControllerSwift/Pods-SampleSlideMenuControllerSwift-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SampleSlideMenuControllerSwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SampleSlideMenuControllerSwiftVersionString[];
+
